564c50ff14533d35c1ef69f2977b5e8f57985861
Sandeepreddy-54 <102615098+Sandeepreddy-54@users.noreply.github.com>
2023-04-04 18:47:33 +0530

    Update anil.sh

--- anil.sh
+++ anil.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2.... $R FAILURE $N"
+        exit 1 
+    else
+        echo -e "$2... $G SUCESS anoooo $N" 
+    fi
+}
+
+
 sudo apt update 
 
+VALIDATE $? "apt updateted"
+
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+VALIDATE $? "cert sucesss"
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
